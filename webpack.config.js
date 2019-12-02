d1bbf8d84832dbfe245b169df922a6381946dba3
sukantamaikap <sukanta.maikap@gmail.com>
2019-12-02 20:54:08 +1100

    adding dependencies and basic liveserver config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const path = require('path');
 module.exports = {
     entry: './src/js/index.js',
     output: {
-        path: path.resolve(__dirname, 'dest/js'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dest'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
     }
 }
